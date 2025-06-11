0a7fac3d101006491d4f7eb791f595bf6eea29a7
Carlos Magno do Nascimento Junior <juninhonasc3@gmail.com>
2025-06-11 15:17:56 -0300

    Atualização da Classe ProdutoService

--- src/main/java/com/project/controleestoque/service/ProdutoService.java
+++ src/main/java/com/project/controleestoque/service/ProdutoService.java
@@ -3,7 +3,9 @@ package com.project.controleestoque.service;
 import com.project.controleestoque.model.Produto;
 import com.project.controleestoque.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -18,22 +20,42 @@ public class ProdutoService {
         if (existente.isPresent()){
             Integer id = existente.get().getId();
             produto.setId(id);
-            registrarEntrada(produto);
+            registrarEntrada(produto.getId(), produto.getQuantidade());
+            return produto;
         }
 
         produtoRepository.save(produto);
         return produto;
     }
 
-    public Produto registrarEntrada(Produto produto){
-        Integer id = produto.getId();
-        Produto produtoEstoque = produtoRepository.getById(id);
-        Integer qtdEstoque = produtoEstoque.getQuantidade();
-        Integer qtdFinal = produto.getQuantidade() + qtdEstoque;
+    public Produto registrarEntrada(Integer id, Integer quantidade){
+        Produto produto = produtoRepository.getById(id);
+        Integer qtdFinal = produto.getQuantidade() + quantidade;
         produto.setQuantidade(qtdFinal);
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    public Produto registrarSaida(Integer id, Integer quantidade) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+
+        if (quantidade <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A quantidade de saída deve ser maior que zero");
+        }
+
+        if (produto.getQuantidade() < quantidade) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT,
+                    "Quantidade em estoque (" + produto.getQuantidade() + ") é insuficiente para saída de " + quantidade);
+        }
+
+        produto.setQuantidade(produto.getQuantidade() - quantidade);
+        return produtoRepository.save(produto);
+    }
+
+    public Produto obterProduto(Integer id){
+        return produtoRepository.getById(id);
     }
 
 }
